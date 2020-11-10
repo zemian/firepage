ab921fa3171e827ef02e78289ba5c30f288fe6a7
Zemian Deng <zemiandeng@gmail.com>
2020-11-10 12:27:23 -0500

    Add Parsedown-Extra lib

--- index.php
+++ index.php
@@ -12,7 +12,7 @@ $config = array(
 );
 
 // Global Vars
-$marknotes_version = '1.2.0';
+$marknotes_version = '1.2.0-SNAPSHOT';
 $marknotes_config_name = '.marknotes.json';
 $marknotes_root_dir = __DIR__;
 
@@ -23,13 +23,6 @@ $marknotes_root_dir = __DIR__;
  * License: The MIT License (MIT)
  * Author: Zemian Deng
  * Date: 2020-11-04
- * 
- * Release Notes:
- * - 1.0.0 2020-10-01 First release!
- * - 1.1.0 2020-11-07 Add nested folders browsing
- * - 1.2.0 Next_release 
- *         * Allow multiple file extension list
- *         * Validate and prevent hidden files/folders creation
  */
 
 //
@@ -294,7 +287,7 @@ if ($form_error === null) {
         if (!isset($file_content)) {
             $file_content = $file_service->read($file);
         }
-        $parsedown = new ParsedownCheckbox();
+        $parsedown = new ParsedownExtra();
         $file_content_formatted = $parsedown->text($file_content);
     } else if ($action !== 'login') {
         $file_content_formatted = "File not found: $file";
@@ -2752,119 +2745,4 @@ class ParsedownExtra extends Parsedown
     protected $regexAttribute = '(?:[#.][-\w]+[ ]*)';
 }
 
-//
-// ### Parsedown Extra Checkbox
-//
-
-/**
- * This file is part of the ParsedownCheckbox package.
- *
- * (c) Simon Leblanc <contact@leblanc-simon.eu>
- *
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
- */
-class ParsedownCheckbox extends ParsedownExtra
-{
-    const VERSION = '0.2.0';
-
-    public function __construct()
-    {
-        parent::__construct();
-
-        array_unshift($this->BlockTypes['['], 'Checkbox');
-    }
-
-    protected function blockCheckbox($line)
-    {
-        $text = trim($line['text']);
-        $begin_line = substr($text, 0, 4);
-        if ('[ ] ' === $begin_line) {
-            return [
-                'handler' => 'checkboxUnchecked',
-                'text' => substr(trim($text), 4),
-            ];
-        }
-
-        if ('[x] ' === $begin_line) {
-            return [
-                'handler' => 'checkboxChecked',
-                'text' => substr(trim($text), 4),
-            ];
-        }
-    }
-
-    protected function blockListComplete(array $block)
-    {
-        foreach ($block['element']['elements'] as &$li_element) {
-            foreach ($li_element['handler']['argument'] as $text) {
-                $begin_line = substr(trim($text), 0, 4);
-                if ('[ ] ' === $begin_line) {
-                    $li_element['attributes'] = ['class' => 'parsedown-task-list parsedown-task-list-open'];
-                } elseif ('[x] ' === $begin_line) {
-                    $li_element['attributes'] = ['class' => 'parsedown-task-list parsedown-task-list-close'];
-                }
-            }
-        }
-
-        return $block;
-    }
-
-    protected function blockCheckboxContinue(array $block)
-    {
-    }
-
-    protected function blockCheckboxComplete(array $block)
-    {
-        $block['element'] = [
-            'rawHtml' => $this->{$block['handler']}($block['text']),
-            'allowRawHtmlInSafeMode' => true,
-        ];
-
-        return $block;
-    }
-
-    protected function checkboxUnchecked($text)
-    {
-        if ($this->markupEscaped || $this->safeMode) {
-            $text = self::escape($text);
-        }
-
-        return '<input type="checkbox" disabled /> ' . $this->format($text);
-    }
-
-    protected function checkboxChecked($text)
-    {
-        if ($this->markupEscaped || $this->safeMode) {
-            $text = self::escape($text);
-        }
-
-        return '<input type="checkbox" checked disabled /> ' . $this->format($text);
-    }
-
-    /**
-     * Formats the checkbox label without double escaping.
-     * @param string $text the string to format
-     * @return string the formatted text
-     */
-    protected function format($text)
-    {
-        // backup settings
-        $markup_escaped = $this->markupEscaped;
-        $safe_mode = $this->safeMode;
-
-        // disable rules to prevent double escaping.
-        $this->setMarkupEscaped(false);
-        $this->setSafeMode(false);
-
-        // format line
-        $text = $this->line($text);
-
-        // reset old values
-        $this->setMarkupEscaped($markup_escaped);
-        $this->setSafeMode($safe_mode);
-
-        return $text;
-    }
-}
 ?>
